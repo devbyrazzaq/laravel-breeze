727d8d19aa88e9353a978da2f0fdd63ab3eecbb6
Fattahur Razzaq <devbyrazzaq@gmail.com>
2023-01-14 20:14:43 +0700

    Showing status Data based on user, Set Status Relationship

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -30,7 +30,7 @@
             @endisset
 
             <!-- Page Content -->
-            <main>
+            <main class="pt-6">
                 {{ $slot }}
             </main>
         </div>
--- resources/views/timeline.blade.php
+++ resources/views/timeline.blade.php
@@ -1,9 +1,26 @@
 <x-app-layout>
 
-    {{-- <x-slot name="header">
-        Timeline
-    </x-slot> --}}
-
-    Timeline
+    <x-container>
+        <div class="grid grid-cols-12 gap-6">
+            <div class="col-span-7">
+                @foreach ($statuses as $status)
+                    <div class="">
+                        <div class="">
+                            <img src="https://i.pravatar.cc/150" alt="{{ $status->user->name }}" srcset="">
+                        </div>
+                        <div class="">
+                            <div class="">{{ $status->user->name }}</div>
+                            <div class="">
+                                {{ $status->body }}
+                            </div>
+                        </div>
+                    </div>
+                @endforeach
+            </div>
+            <div class="col-span-5">
+                Friend
+            </div>
+        </div>
+    </x-container>
 
 </x-app-layout>
